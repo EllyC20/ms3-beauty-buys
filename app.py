c8a9646731cb9e6d0ac4a950d7772d573ad75982
EllyC20 <callanelly@yahoo.com>
2021-03-26 15:05:55 +0000

    feat: Add Navbar functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,6 +19,11 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/get_reviews")
 def get_reviews():
     reviews = mongo.db.product_detail.find()
