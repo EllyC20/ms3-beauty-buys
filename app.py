07fb57eb2b0014baf1f6c00795eaabf5ffa08d5e
EllyC20 <callanelly@yahoo.com>
2021-03-29 15:33:20 +0000

    feat: Add login functionality

--- app.py
+++ app.py
@@ -47,6 +47,31 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if username exists in the db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Ensure password match
+            if check_password_hash(
+               existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # Wrong password
+                flash("Incorrect Username And/Or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # Username doesn't exist
+            flash("Incorrect Username And/Or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
 @app.route("/get_reviews")
 def get_reviews():
     reviews = mongo.db.product_detail.find()
