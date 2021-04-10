842a71baa208054b96ca6414500e67bd51d32ea2
EllyC20 <callanelly@yahoo.com>
2021-04-10 14:00:55 +0000

    feat: Initialise modal

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.carousel').carousel();
     $('select').formSelect();
+    $('.modal').modal();
 });
 
 $('.carousel').carousel({
